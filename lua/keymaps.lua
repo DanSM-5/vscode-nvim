0f873449e8e59996ec961dc580aa63983d426f64
daniel <eduardo.daniel.s.m@gmail.com>
2024-11-20 22:41:46 -0500

    feat(maps): Update keymaps
    - Add maps to navigate symbols
    - Format keymaps.lua file

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,20 +2,19 @@ local vscode = require('vscode')
 local nx = { 'n', 'x' }
 
 return {
-  set_default = function ()
-
+  set_default = function()
     -- Vim commentary emulation
     vim.keymap.set('x', 'gc', '<Plug>VSCodeCommentary', {
-      desc = '[VSCommentary]: Start comment action with word objects'
+      desc = '[VSCommentary]: Start comment action with word objects',
     })
     vim.keymap.set('n', 'gc', '<Plug>VSCodeCommentary', {
-      desc = '[VSCommentary]: Start comment action with word objects'
+      desc = '[VSCommentary]: Start comment action with word objects',
     })
     vim.keymap.set('o', 'gc', '<Plug>VSCodeCommentary', {
-      desc = '[VSCommentary]: Start comment action with word objects'
+      desc = '[VSCommentary]: Start comment action with word objects',
     })
     vim.keymap.set('n', 'gcc', '<Plug>VSCodeCommentaryLine', {
-      desc = '[VSCommentary]: Toggle comment line under the cursor'
+      desc = '[VSCommentary]: Toggle comment line under the cursor',
     })
 
     -- Ctrl+Shift+Up/Down to move up and down
@@ -33,46 +32,41 @@ return {
     --   { desc = 'Move line under the cursor up', silent = true })
 
     -- ]<End> or ]<Home> move current line to the end or the begin of current buffer
-    vim.keymap.set('n', ']<End>', 'ddGp``',
-      { desc = 'Move line to end of the buffer', noremap = true, silent = true })
-    vim.keymap.set('n', ']<Home>', 'ddggP``',
-      { desc = 'Move line to start of the buffer', noremap = true, silent = true })
-    vim.keymap.set('v', ']<End>', 'dGp``',
-      { desc = 'Move line to end of the buffer', noremap = true, silent = true })
-    vim.keymap.set('v', ']<Home>', 'dggP``',
-      { desc = 'Move line to start of the buffer', noremap = true, silent = true })
+    vim.keymap.set('n', ']<End>', 'ddGp``', { desc = 'Move line to end of the buffer', noremap = true, silent = true })
+    vim.keymap.set(
+      'n',
+      ']<Home>',
+      'ddggP``',
+      { desc = 'Move line to start of the buffer', noremap = true, silent = true }
+    )
+    vim.keymap.set('v', ']<End>', 'dGp``', { desc = 'Move line to end of the buffer', noremap = true, silent = true })
+    vim.keymap.set(
+      'v',
+      ']<Home>',
+      'dggP``',
+      { desc = 'Move line to start of the buffer', noremap = true, silent = true }
+    )
 
     -- Select blocks after indenting
-    vim.keymap.set('x', '<', '<gv',
-      { desc = 'Reselect visual block after reducing indenting', noremap = true })
-    vim.keymap.set('x', '>', '>gv|',
-      { desc = 'Reselect visual block after increasing indenting', noremap = true })
+    vim.keymap.set('x', '<', '<gv', { desc = 'Reselect visual block after reducing indenting', noremap = true })
+    vim.keymap.set('x', '>', '>gv|', { desc = 'Reselect visual block after increasing indenting', noremap = true })
 
     -- Use tab for indenting in visual mode
-    vim.keymap.set('x', '<Tab>', '>gv|',
-      { desc = 'Increase indentation of selected block', noremap = true })
-    vim.keymap.set('x', '<S-Tab>', '<gv',
-      { desc = 'Decrease indentation of selected block', noremap = true })
-    vim.keymap.set('n', '>', '>>_',
-      { desc = 'Increase indentation of line', noremap = true })
-    vim.keymap.set('n', '<', '<<_',
-      { desc = 'Decrease indentation of line', noremap = true })
+    vim.keymap.set('x', '<S-Tab>', '<gv', { desc = 'Decrease indentation of selected block', noremap = true })
+    vim.keymap.set('x', '<Tab>', '>gv|', { desc = 'Increase indentation of selected block', noremap = true })
+    vim.keymap.set('n', '>', '>>_', { desc = 'Increase indentation of line', noremap = true })
+    vim.keymap.set('n', '<', '<<_', { desc = 'Decrease indentation of line', noremap = true })
 
     -- smart up and down
-    vim.keymap.set('n', '<down>', 'gj',
-      { desc = 'Move down in wrapped lines', silent = true, remap = true })
-    vim.keymap.set('n', '<up>', 'gk',
-      { desc = 'Move up in wrapped lines', silent = true, remap = true })
+    vim.keymap.set('n', '<down>', 'gj', { desc = 'Move down in wrapped lines', silent = true, remap = true })
+    vim.keymap.set('n', '<up>', 'gk', { desc = 'Move up in wrapped lines', silent = true, remap = true })
     -- nnoremap <expr> k (v:count == 0 ? 'gk' : 'k')
     -- nnoremap <expr> j (v:count == 0 ? 'gj' : 'j')
 
     -- Fast saving
-    vim.keymap.set('n', '<C-s>', ':<C-u>w<CR>',
-      { desc = 'Save buffer with ctrl-s', noremap = true })
-    vim.keymap.set('v', '<C-s>', ':<C-u>w<CR>',
-      { desc = 'Save buffer with ctrl-s', noremap = true })
-    vim.keymap.set('c', '<C-s>', '<C-u>w<CR>',
-      { desc = 'Save buffer with ctrl-s', noremap = true })
+    vim.keymap.set('n', '<C-s>', ':<C-u>w<CR>', { desc = 'Save buffer with ctrl-s', noremap = true })
+    vim.keymap.set('v', '<C-s>', ':<C-u>w<CR>', { desc = 'Save buffer with ctrl-s', noremap = true })
+    vim.keymap.set('c', '<C-s>', '<C-u>w<CR>', { desc = 'Save buffer with ctrl-s', noremap = true })
 
     -- SystemCopy keybindings
     vim.keymap.set('n', 'zy', '<Plug>SystemCopy', {
@@ -115,36 +109,31 @@ return {
     vim.cmd('source ' .. '~/vim-config/utils/clipboard.vim')
 
     -- Map clipboard functions
-    vim.keymap.set('x', '<Leader>y', ':<C-u>call clipboard#yank()<cr>',
-      {
-        desc = 'Yank selection to system clipboard',
-        silent = true,
-        noremap = true
-      })
-    vim.keymap.set('n', '<Leader>p', 'clipboard#paste("p")',
-      {
-        desc = 'Paste content of system clipboard after the cursor',
-        expr = true,
-        noremap = true
-      })
-    vim.keymap.set('n', '<Leader>P', 'clipboard#paste("P")',
-      {
-        desc = 'Paste content of system clipboard after the cursor',
-        expr = true,
-        noremap = true
-      })
-    vim.keymap.set('x', '<Leader>p', 'clipboard#paste("p")',
-      {
-        desc = 'Paste content of system clipboard after the cursor',
-        expr = true,
-        noremap = true
-      })
-    vim.keymap.set('x', '<Leader>P', 'clipboard#paste("P")',
-      {
-        desc = 'Paste content of system clipboard after the cursor',
-        expr = true,
-        noremap = true
-      })
+    vim.keymap.set('x', '<Leader>y', ':<C-u>call clipboard#yank()<cr>', {
+      desc = 'Yank selection to system clipboard',
+      silent = true,
+      noremap = true,
+    })
+    vim.keymap.set('n', '<Leader>p', 'clipboard#paste("p")', {
+      desc = 'Paste content of system clipboard after the cursor',
+      expr = true,
+      noremap = true,
+    })
+    vim.keymap.set('n', '<Leader>P', 'clipboard#paste("P")', {
+      desc = 'Paste content of system clipboard after the cursor',
+      expr = true,
+      noremap = true,
+    })
+    vim.keymap.set('x', '<Leader>p', 'clipboard#paste("p")', {
+      desc = 'Paste content of system clipboard after the cursor',
+      expr = true,
+      noremap = true,
+    })
+    vim.keymap.set('x', '<Leader>P', 'clipboard#paste("P")', {
+      desc = 'Paste content of system clipboard after the cursor',
+      expr = true,
+      noremap = true,
+    })
 
     -- Clean trailing whitespace in file
     vim.keymap.set('n', '<Leader>cc', ':%s/\\s\\+$//e<cr>', {
@@ -164,43 +153,30 @@ return {
       noremap = true,
     })
 
-    -- " Move between buffers with tab
-    -- NOTE: disabled for issues on vscode handling of buffer next/prev
-    -- vim.keymap.set('n', '<tab>', ':bn<cr>', {
-    --   desc = 'Move to next buffer',
-    --   noremap = true,
-    --   silent = true,
-    -- })
-    -- vim.keymap.set('n', '<s-tab>', ':bN<cr>', {
-    --   desc = 'Move to previous buffer',
-    --   noremap = true,
-    --   silent = true,
-    -- })
-
     -- vim-asterisk
     vim.keymap.set({ 'n', 'v', 'o' }, '*', '<Plug>(asterisk-*)', {
-      desc = '[Asterisk] Select word under the cursor *'
+      desc = '[Asterisk] Select word under the cursor *',
     })
     vim.keymap.set({ 'n', 'v', 'o' }, '#', '<Plug>(asterisk-#)', {
-      desc = '[Asterisk] Select word under the cursor #'
+      desc = '[Asterisk] Select word under the cursor #',
     })
     vim.keymap.set({ 'n', 'v', 'o' }, 'g*', '<Plug>(asterisk-g*)', {
-      desc = '[Asterisk] Select word under the cursor g*'
+      desc = '[Asterisk] Select word under the cursor g*',
     })
     vim.keymap.set({ 'n', 'v', 'o' }, 'g#', '<Plug>(asterisk-g#)', {
-      desc = '[Asterisk] Select word under the cursor g#'
+      desc = '[Asterisk] Select word under the cursor g#',
     })
     vim.keymap.set({ 'n', 'v', 'o' }, 'z*', '<Plug>(asterisk-z*)', {
-      desc = '[Asterisk] Select word under the cursor * (preserve position)'
+      desc = '[Asterisk] Select word under the cursor * (preserve position)',
     })
     vim.keymap.set({ 'n', 'v', 'o' }, 'gz*', '<Plug>(asterisk-gz*)', {
-      desc = '[Asterisk] Select word under the cursor # (preserve position)'
+      desc = '[Asterisk] Select word under the cursor # (preserve position)',
     })
     vim.keymap.set({ 'n', 'v', 'o' }, 'z#', '<Plug>(asterisk-z#)', {
-      desc = '[Asterisk] Select word under the cursor g* (preserve position)'
+      desc = '[Asterisk] Select word under the cursor g* (preserve position)',
     })
     vim.keymap.set({ 'n', 'v', 'o' }, 'gz#', '<Plug>(asterisk-gz#)', {
-      desc = '[Asterisk] Select word under the cursor g# (preserve position)'
+      desc = '[Asterisk] Select word under the cursor g# (preserve position)',
     })
 
     -- Set 'stay' behavior by default
@@ -222,7 +198,6 @@ return {
     -- nnoremap <S-Up> <S-Up>zz
     -- nnoremap <S-Down> <S-Down>zz
 
-
     -- Vscode actions -- LSP like bindings
 
     -- Go to implementation mappings
@@ -238,7 +213,7 @@ return {
       desc = '[VSCode] Peak implementations',
       noremap = true,
     })
-    vim.keymap.set('n', '<space>rn', function ()
+    vim.keymap.set('n', '<space>rn', function()
       vscode.action('editor.action.rename')
     end, {
       desc = '[VSCode] Rename symbol',
@@ -261,7 +236,6 @@ return {
       noremap = true,
     })
 
-
     -- Open reference in vertical split
     vim.keymap.set('n', 'gv', function()
       vscode.action('editor.action.revealDefinitionAside')
@@ -281,35 +255,35 @@ return {
     -- end, { desc = '[VSCode] Go to previous change', noremap = true })
 
     -- Hunk stage
-    vim.keymap.set('n', '<leader>hs', function ()
+    vim.keymap.set('n', '<leader>hs', function()
       vscode.action('git.stageChange')
     end, { desc = '[VSCode] Stage hunk', noremap = true })
-    vim.keymap.set('v', '<leader>hs', function ()
+    vim.keymap.set('v', '<leader>hs', function()
       vscode.action('git.stageSelectedRanges')
     end, { desc = '[VSCode] Stage hunk', noremap = true })
 
     -- Hunk undo stage
-    vim.keymap.set('n', '<leader>hu', function ()
+    vim.keymap.set('n', '<leader>hu', function()
       -- vscode.action('git.stageChange')
       vscode.action('git.unstageSelectedRanges')
     end, { desc = '[VSCode] Stage hunk', noremap = true })
-    vim.keymap.set('v', '<leader>hu', function ()
+    vim.keymap.set('v', '<leader>hu', function()
       vscode.action('git.unstageSelectedRanges')
     end, { desc = '[VSCode] Stage hunk', noremap = true })
 
     -- Hunk reset
-    vim.keymap.set('n', '<leader>hr', function ()
+    vim.keymap.set('n', '<leader>hr', function()
       vscode.action('git.revertChange')
     end, { desc = '[VSCode] Revert hunk', noremap = true })
-    vim.keymap.set('v', '<leader>hr', function ()
+    vim.keymap.set('v', '<leader>hr', function()
       vscode.action('git.revertSelectedRanges')
     end, { desc = '[VSCode] Revert hunk', noremap = true })
 
     -- Hunk preview
-    vim.keymap.set('n', '<leader>hp', function ()
+    vim.keymap.set('n', '<leader>hp', function()
       vscode.action('editor.action.dirtydiff.next')
     end, { desc = '[VSCode] Preview hunk', noremap = true })
-    vim.keymap.set('n', '<leader>hP', function ()
+    vim.keymap.set('n', '<leader>hP', function()
       vscode.action('editor.action.dirtydiff.previous')
     end, { desc = '[VSCode] Preview hunk', noremap = true })
 
@@ -383,7 +357,6 @@ return {
       noremap = true,
     })
 
-
     -- Fold Toggle
     vim.keymap.set('n', 'za', function()
       vscode.action('editor.toggleFold')
@@ -401,38 +374,37 @@ return {
     --     endfor
     -- endfunction
 
-    vim.keymap.set(nx, '<A-j>', function ()
+    vim.keymap.set(nx, '<A-j>', function()
       vscode.action('workbench.action.navigateDown')
     end, {
-      desc = '[VSCode] Navigate to down window'
+      desc = '[VSCode] Navigate to down window',
     })
-    vim.keymap.set(nx, '<A-k>', function ()
+    vim.keymap.set(nx, '<A-k>', function()
       vscode.action('workbench.action.navigateUp')
     end, {
-      desc = '[VSCode] Navigate to up window'
+      desc = '[VSCode] Navigate to up window',
     })
-    vim.keymap.set(nx, '<A-h>', function ()
+    vim.keymap.set(nx, '<A-h>', function()
       vscode.action('workbench.action.navigateLeft')
     end, {
-      desc = '[VSCode] Navigate to left window'
+      desc = '[VSCode] Navigate to left window',
     })
-    vim.keymap.set(nx, '<A-l>', function ()
+    vim.keymap.set(nx, '<A-l>', function()
       vscode.action('workbench.action.navigateRight')
     end, {
-      desc = '[VSCode] Navigate to right window'
+      desc = '[VSCode] Navigate to right window',
     })
 
     -- Move next buffer
-    vim.keymap.set('n', '<tab>', function ()
+    vim.keymap.set('n', '<Tab>', function()
       -- vscode.action('workbench.action.nextEditor')
       vscode.action('workbench.action.nextEditorInGroup')
     end, { desc = '[VSCode] Move to next buffer', noremap = true })
     -- Move to previous buffer
-    vim.keymap.set('n', '<s-tab>', function ()
+    vim.keymap.set('n', '<s-tab>', function()
       -- vscode.action('workbench.action.previousEditor')
       vscode.action('workbench.action.previousEditorInGroup')
     end, { desc = '[VSCode] Move to previous buffer', noremap = true })
-
 
     -- Clean carriage returns '^M'
     vim.keymap.set('n', '<leader>cr', ':%s/\r$//g<cr>', {
@@ -440,53 +412,59 @@ return {
       noremap = true,
       silent = true,
     })
+
+    local select_symbol = function()
+      vscode.action('workbench.action.gotoSymbol')
+    end
+    vim.keymap.set('n', '<leader>ss', select_symbol, { noremap = true, desc = '[VSCode] Select symbol' })
+    vim.keymap.set('n', '<leader>fa', select_symbol, { noremap = true, desc = '[VSCode] Select symbol' })
   end,
 
-  set_repeatable = function ()
+  set_repeatable = function()
     local repeat_motion = require('utils.repeat_motion')
     repeat_motion.set_motion_keys()
     local repeat_pair = repeat_motion.repeat_pair
 
     -- Hunk next
-    local nextChange = function ()
+    local nextChange = function()
       vscode.action('workbench.action.editor.nextChange')
     end
     -- Hunk prev
-    local prevChange = function ()
+    local prevChange = function()
       vscode.action('workbench.action.editor.previousChange')
     end
 
     repeat_pair({
       keys = 'c',
       desc_forward = '[VSCode] Go to next change',
-      desc_backward =  '[VSCode] Go to previous change',
+      desc_backward = '[VSCode] Go to previous change',
       on_forward = nextChange,
       on_backward = prevChange,
     })
 
     -- Go to next merge conflict
-    local nextConflict = function ()
+    local nextConflict = function()
       vscode.action('merge-conflict.next')
     end
     -- Go to prev merge conflict
-    local prevConflict = function ()
+    local prevConflict = function()
       vscode.action('merge-conflict.previous')
     end
 
     repeat_pair({
       keys = 'n',
       desc_forward = '[VSCode] Go to next merge conflict',
-      desc_backward =  '[VSCode] Go to prev merge conflict',
+      desc_backward = '[VSCode] Go to prev merge conflict',
       on_forward = nextConflict,
       on_backward = prevConflict,
     })
 
     -- Diagnostic next
-    local nextDiagnostic = function ()
+    local nextDiagnostic = function()
       vscode.action('editor.action.marker.next')
     end
     -- Diagnostic prev
-    local prevDiagnostic = function ()
+    local prevDiagnostic = function()
       vscode.action('editor.action.marker.prev')
     end
 
@@ -506,11 +484,11 @@ return {
     })
 
     -- Next ocurrence of symbol
-    local nextSymbol = function ()
+    local nextSymbol = function()
       vscode.action('editor.action.wordHighlight.next')
     end
     -- Previous ocurrence of symbol
-    local prevSymbol = function ()
+    local prevSymbol = function()
       vscode.action('editor.action.wordHighlight.prev')
     end
 
@@ -524,13 +502,12 @@ return {
 
     -- NOTE: Requires extension "Go to Next/Previous Member"
     -- Next member
-    local nextMember = function ()
+    local nextMember = function()
       vscode.action('gotoNextPreviousMember.nextMember')
     end
-    local prevMember = function ()
+    local prevMember = function()
       vscode.action('gotoNextPreviousMember.previousMember')
     end
-
 
     repeat_pair({
       keys = 'a',
@@ -550,37 +527,37 @@ return {
 
     local create_repeatable_pair = repeat_motion.create_repeatable_pair
 
-    local vsplit_bigger, vsplit_smaller  = create_repeatable_pair(function ()
+    local vsplit_bigger, vsplit_smaller = create_repeatable_pair(function()
       manageEditorSize(vim.v.count, 'workbench.action.increaseViewWidth')
-    end, function ()
+    end, function()
       manageEditorSize(vim.v.count, 'workbench.action.decreaseViewWidth')
     end)
 
     -- Window resize vsplit
     vim.keymap.set(nx, '<A-.>', vsplit_bigger, {
       desc = '[VSCode] Increase editor window width',
-      noremap = true
+      noremap = true,
     })
     vim.keymap.set(nx, '<A-,>', vsplit_smaller, {
       desc = '[VSCode] Decrease editor window width',
-      noremap = true
+      noremap = true,
     })
 
-    local split_bigger, split_smaller = create_repeatable_pair(function ()
+    local split_bigger, split_smaller = create_repeatable_pair(function()
       manageEditorSize(vim.v.count, 'workbench.action.increaseViewHeight')
-    end, function ()
+    end, function()
       manageEditorSize(vim.v.count, 'workbench.action.decreaseViewHeight')
     end)
 
     -- Window resize split
     vim.keymap.set(nx, '<A-t>', split_bigger, {
       desc = '[VSCode] Increase editor window height',
-      noremap = true
+      noremap = true,
     })
     vim.keymap.set(nx, '<A-s>', split_smaller, {
       desc = '[VSCode] Decrease editor window height',
-      noremap = true
+      noremap = true,
     })
-  end
+  end,
 }
 
