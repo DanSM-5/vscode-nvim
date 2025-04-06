7c72dd83bdec18f8615a77a2b98243b818ccb09c
daniel <eduardo.daniel.s.m@gmail.com>
2025-04-06 16:25:00 -0400

    feat(gitvscode): Improve the way hunks are find using vscode itself

--- lua/utils/gitvscode.lua
+++ lua/utils/gitvscode.lua
@@ -2,12 +2,29 @@
 ---[ baseStart, baseRange, newStart, newRange ]
 ---@alias Hunk { baseStart: integer, baseRange: integer, newStart: integer, newRange: integer } Represents a hunk from git cli
 
+--- VsCodeHunkKind
+--- [1] 1 Added
+--- [2] 2 Deleted
+--- [3] 3 Modified
+
+---@class VsCodeHunkRange
+---@field startLineNumber integer Starting of the hunk
+---@field endLineNumberExclusive integer End of hunk exclusive
+
+---@class VsCodeHunk
+---@field original VsCodeHunkRange Range of the original file
+---@field modified VsCodeHunkRange Range of the modified file
+---@field kind 1|2|3 Kind value. See VsCodeHunkKind
+
+
 -- NOTE: Commands fail on windows when it meets the following conditions:
 -- - vscode-neovim extension uses the `nvim` binary from windows
 -- - vscode is working using the remote extension e.g. WSL
 -- - awk binary from scoop (gawk package) is used (gitbash awk is not afected).
 -- TODO: Rework commands in windows to use a different solution like powershell
 -- or a binary that is reliable under the windows environment.
+-- There are still limitations like the need to allow git to work on directories with
+-- different permissions (trusts directories).
 local unstaged_hunk_command = "git -C %s diff %s | awk '$0 ~ /^@@/ { print substr($2, 2)\",\"substr($3, 2) }'"
 local staged_hunk_command = "git -C %s diff --cached %s | awk '$0 ~ /^@@/ { print substr($2, 2)\",\"substr($3, 2) }'"
 
@@ -42,6 +59,122 @@ local is_cursor_in_hunk = function (hunk)
 
   return false
 end
+
+-- TODO: Check vscode.window.activeTextEditor.diffInformation
+-- Sample:
+-- local a = {
+--   {
+--     changes = { {
+--         kind = 1,
+--         modified = {
+--           endLineNumberExclusive = 491,
+--           startLineNumber = 490
+--         },
+--         original = {
+--           endLineNumberExclusive = 490,
+--           startLineNumber = 490
+--         }
+--       } },
+--     documentVersion = 201,
+--     isStale = false,
+--     modified = {
+--       ["$mid"] = 1,
+--       _sep = 1,
+--       external = "file:///c%3A/Users/daniel/projects/tabs-to-links/action/script.js",
+--       fsPath = "c:\\Users\\daniel\\projects\\tabs-to-links\\action\\script.js",
+--       path = "/c:/Users/daniel/projects/tabs-to-links/action/script.js",
+--       scheme = "file"
+--     },
+--     original = {
+--       ["$mid"] = 1,
+--       external = "git:/c%3A/Users/daniel/projects/tabs-to-links/action/script.js.git?%7B%22path%22%3A%22c%3A%5C%5CUsers%5C%5Cdaniel%5C%5Cprojects%5C%5Ctabs-to-links%5C%5Caction%5C%5Cscript.js%22%2C%22ref%22%3A%22%22%7D",
+--       path = "/c:/Users/daniel/projects/tabs-to-links/action/script.js.git",
+--       query = '{"path":"c:\\\\Users\\\\daniel\\\\projects\\\\tabs-to-links\\\\action\\\\script.js","ref":""}',
+--       scheme = "git"
+--     }
+--   }
+-- }
+
+---Get the unstaged hunk from vscode
+---This is identified as the one with query without ref
+---@return VsCodeHunk? Hunk if available
+local get_unstaged_hunk_under_cursor_js = function ()
+  -- vscode hunk lines are 1-based indexed
+  local line = vim.fn.line('.')
+
+  -- Try to get the overlaping line in the hunk from diffInformation
+
+  ---@type VsCodeHunk?
+  local vsc_hunk = require('vscode').eval([[
+    const changes = vscode.window?.activeTextEditor?.diffInformation?.find(di => {
+      const query = JSON.parse(di.original.query)
+      return query.ref === ''
+    })?.changes ||
+      vscode.window?.activeTextEditor?.diffInformation?.[0]?.changes ||
+      vscode.window?.activeTextEditor?.diffInformation?.[1]?.changes
+
+    const hunk = changes?.find(h => {
+      return h.modified.startLineNumber <= args.line && h.modified.endLineNumberExclusive >= args.line
+    })
+
+    logger.info('Found hunk:', hunk)
+
+    return hunk
+  ]], { args = { line = line } })
+
+  -- We don't want to return vim.NIL but lua's nil
+  if vsc_hunk == vim.NIL or not vsc_hunk then
+    return
+  end
+
+  return vsc_hunk
+
+  -- return {
+  --   newStart = vsc_hunk.modified.startLineNumber,
+  --   newRange = vsc_hunk.modified.endLineNumberExclusive - vsc_hunk.modified.startLineNumber,
+  --   baseStart = vsc_hunk.original.startLineNumber,
+  --   baseRange = vsc_hunk.original.endLineNumberExclusive - vsc_hunk.original.startLineNumber,
+  -- }
+end
+
+---Get the hunk from vscode
+---This function checks for diffInformation\[1] preferably, if not present,
+---tries diffInformation\[0].
+---@return VsCodeHunk? Hunk if available
+local get_hunk_under_cursor_js = function ()
+  -- vscode hunk lines are 1-based indexed
+  local line = vim.fn.line('.')
+
+  -- Try to get the overlaping line in the hunk from diffInformation
+
+  ---@type VsCodeHunk?
+  local vsc_hunk = require('vscode').eval([[
+    const changes = vscode.window?.activeTextEditor?.diffInformation?.find(di => {
+      const query = JSON.parse(di.original.query)
+      return query.ref === 'HEAD'
+    })?.changes ||
+      vscode.window?.activeTextEditor?.diffInformation?.[1]?.changes ||
+      vscode.window?.activeTextEditor?.diffInformation?.[0]?.changes
+
+    const hunk = changes?.find(h => {
+      return h.modified.startLineNumber <= args.line && h.modified.endLineNumberExclusive >= args.line
+    })
+
+    logger.info('Found hunk:', hunk)
+
+    return hunk
+  ]], { args = { line = line } })
+
+  -- We don't want to return vim.NIL but lua's nil
+  if vsc_hunk == vim.NIL or not vsc_hunk then
+    return
+  end
+
+  return vsc_hunk
+end
+
+-- VSCode documentation
+-- https://vscode-api.js.org/classes/vscode.Diagnostic.html
 
 -- //wsl.localhost/UbuntuDev/home/eduardo/projects/nbcu_main_mw
 -- vscode-remote://wsl%2Bubuntudev/home/eduardo/projects/nbcu_main_mw
@@ -120,7 +253,7 @@ end
 ---Get the current hunk under the cursor
 ---@param staged? boolean Whether or not get the staged hunks
 ---@return Hunk? The hunk under the cursor or nil if non is found
-local get_hunk_under_cursor = function (staged)
+local get_hunk_under_cursor_cli = function (staged)
   local file = get_file()
   -- We need a filename for get_hunks
   local dir = vim.fn.fnamemodify(file, ':p:h')
@@ -135,11 +268,24 @@ local get_hunk_under_cursor = function (staged)
   end
 end
 
-local stage_hunk_under_cursor_vscode = function ()
-  -- local file = get_file()
-  -- return get_hunks(file)
+-- NOTE: First try extracting hunk from vscdoe, then fallback to git cli
+-- Hunk info from vscode is faster and more accurate
+-- Unstage hunks seems not to be possible from the regular editor and seems to be more
+-- of a feature of some diff view.
 
-  local hunk = get_hunk_under_cursor()
+local stage_hunk_under_cursor_vscode = function ()
+  local vscode_hunk = get_unstaged_hunk_under_cursor_js()
+  if vscode_hunk then
+    require('vscode')
+      .action('git.stageSelectedRanges', {
+        range = { vscode_hunk.modified.startLineNumber - 1, vscode_hunk.modified.endLineNumberExclusive - 1 },
+        restore_selection = true,
+      })
+    return
+  end
+
+
+  local hunk = get_hunk_under_cursor_cli()
   if not hunk then
     return
   end
@@ -156,7 +302,17 @@ local stage_hunk_under_cursor_vscode = function ()
 end
 
 local unstage_hunk_under_cursor_vscode = function ()
-  local hunk = get_hunk_under_cursor(true)
+  local vscode_hunk = get_hunk_under_cursor_js()
+  if vscode_hunk then
+    require('vscode')
+      .action('git.unstageSelectedRanges', {
+        range = { vscode_hunk.modified.startLineNumber - 1, vscode_hunk.modified.endLineNumberExclusive - 1 },
+        restore_selection = true,
+      })
+    return
+  end
+
+  local hunk = get_hunk_under_cursor_cli(true)
   if not hunk then
     return
   end
@@ -169,7 +325,17 @@ local unstage_hunk_under_cursor_vscode = function ()
 end
 
 local revert_hunk_under_cursor_vscode = function ()
-  local hunk = get_hunk_under_cursor()
+  local vscode_hunk = get_unstaged_hunk_under_cursor_js()
+  if vscode_hunk then
+    require('vscode')
+      .action('git.revertSelectedRanges', {
+        range = { vscode_hunk.modified.startLineNumber - 1, vscode_hunk.modified.endLineNumberExclusive - 1 },
+        restore_selection = true,
+      })
+    return
+  end
+
+  local hunk = get_hunk_under_cursor_cli()
   if not hunk then
     return
   end
@@ -177,7 +343,7 @@ local revert_hunk_under_cursor_vscode = function ()
   require('vscode')
     .action('git.revertSelectedRanges', {
       range = { hunk.newStart - 1, hunk.newStart - 1 + hunk.newRange },
-      restore_selection = false,
+      restore_selection = true,
     })
 end
 
@@ -280,7 +446,7 @@ end
 return {
   is_cursor_in_hunk = is_cursor_in_hunk,
   get_hunks = get_hunks,
-  get_hunk_under_cursor = get_hunk_under_cursor,
+  get_hunk_under_cursor = get_hunk_under_cursor_cli,
   get_file = get_file,
   stage_hunk_under_cursor = stage_hunk_under_cursor,
   stage_hunk_under_cursor_vscode = stage_hunk_under_cursor_vscode,
