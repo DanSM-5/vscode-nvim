13163837adc595617072d93583e72ced1898e63a
daniel <eduardo.daniel.s.m@gmail.com>
2025-04-12 00:01:05 -0400

    feat(plugins): Update configs

--- lua/plugins_vscode/code.lua
+++ lua/plugins_vscode/code.lua
@@ -34,16 +34,21 @@ return {
   {
     'echasnovski/mini.ai',
     event = 'VeryLazy',
-    opts = {
-      search_method = 'cover',
-      n_lines = 999999,
-      custom_textobjects = {
-        ["'"] = false,
-        ['"'] = false,
-        ['`'] = false,
-        -- ['f'] = false,
-      },
-    },
+    config = function ()
+      local ai = require('mini.ai')
+      ai.setup({
+        search_method = 'cover',
+        n_lines = 999999,
+        custom_textobjects = {
+          ["'"] = false,
+          ['"'] = false,
+          ['`'] = false,
+          -- Set function call to F
+          ['f'] = false,
+          ['F'] = ai.gen_spec.function_call(),
+        },
+      })
+    end,
   },
   {
     'nvim-treesitter/nvim-treesitter-textobjects',
