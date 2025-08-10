2587cb1efdf11be9306cfcb26521b5ebd0dda77c
daniel <eduardo.daniel.s.m@gmail.com>
2025-08-10 16:44:06 -0400

    refactor(lsp): disable cmp2lsp in process lsp

--- lua/config/nvim.lua
+++ lua/config/nvim.lua
@@ -12,9 +12,15 @@ vim.g.markdown_folding = 1
 
 
 local lsp_servers = {}
+local ignore_servers = { 'cmp2lsp' }
 for _, file in ipairs(vim.api.nvim_get_runtime_file('after/lsp/*', true)) do
   local name = vim.fn.fnamemodify(file, ':t:r')
+  if vim.tbl_contains(ignore_servers, name) then
+    goto continue
+  end
   table.insert(lsp_servers, name)
+
+  ::continue::
 end
 -- Start lsps
 vim.lsp.enable(lsp_servers)
