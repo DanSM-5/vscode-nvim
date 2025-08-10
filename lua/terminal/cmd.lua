4c4568a0f0961cd66bb0431d4dc8e9212ff2f8fc
daniel <eduardo.daniel.s.m@gmail.com>
2025-08-10 12:14:32 -0400

    feat(cmd): Add :Redir command to save output to a temporary buffer

--- lua/terminal/cmd.lua
+++ lua/terminal/cmd.lua
@@ -152,6 +152,14 @@ local register = function()
     force = true,
     desc = '[Git] Open fzf in top git repo or active buffer directory',
   })
+
+  vim.api.nvim_create_user_command('Redir', function(ctx)
+    -- local lines = vim.split(vim.api.nvim_exec(ctx.args, true), '\n', { plain = true })
+    local lines = vim.split(vim.api.nvim_exec2(ctx.args, { output = true }).output or '', '\n', { plain = true })
+    vim.cmd('new')
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+    vim.opt_local.modified = false
+  end, { nargs = '+', complete = 'command' })
 end
 
 return {
