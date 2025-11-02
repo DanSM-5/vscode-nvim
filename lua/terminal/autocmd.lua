f033b79776d7a9db1730912d1ec13d1a735a3397
daniel <eduardo.daniel.s.m@gmail.com>
2025-11-01 22:13:05 -0400

    style(autocmd): apply formatting

--- lua/terminal/autocmd.lua
+++ lua/terminal/autocmd.lua
@@ -5,17 +5,16 @@ local register = function()
     pattern = 'fugitive',
     desc = '[fugitive] Bring back the "a" keymap',
     callback = function(opts)
-      vim.keymap.set('n', 'a', function ()
+      vim.keymap.set('n', 'a', function()
         return '-'
-      end,
-        {
-          silent = true,
-          remap = true,
-          expr = true,
-          desc = '[fugitive] Toggle file staging',
-          buffer = opts.buf
-        })
-    end
+      end, {
+        silent = true,
+        remap = true,
+        expr = true,
+        desc = '[fugitive] Toggle file staging',
+        buffer = opts.buf,
+      })
+    end,
   })
 
   local custom_term = vim.api.nvim_create_augroup('custom_term', { clear = true })
@@ -36,12 +35,11 @@ local register = function()
     desc = 'Recover previous cursor position in buffer',
     pattern = { '*' },
     callback = function()
-      if (vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$")) then
-        vim.fn.execute("normal! g`\"zz")
+      if vim.fn.line('\'"') > 0 and vim.fn.line('\'"') <= vim.fn.line('$') then
+        vim.fn.execute('normal! g`"zz')
       end
-    end
+    end,
   })
-
 
   -- Change cursor color when recording a macro as a visual help
   local record_group = vim.api.nvim_create_augroup('CursorColorOnRecord', { clear = true })
@@ -49,32 +47,31 @@ local register = function()
   vim.api.nvim_create_autocmd('RecordingEnter', {
     desc = 'Change cursor color when recording macro starts',
     group = record_group,
-    callback = function ()
+    callback = function()
       recover_cursor_color = vim.api.nvim_get_hl(0, { name = 'Cursor' })
       -- Set cursor to green to signal that recording started
       vim.api.nvim_set_hl(0, 'Cursor', { fg = '#282c34', bg = '#c678dd', ctermfg = 0, ctermbg = 040 })
-    end
+    end,
   })
   vim.api.nvim_create_autocmd('RecordingLeave', {
     desc = 'Recover cursor color when recording macro starts',
     group = record_group,
-    callback = function ()
+    callback = function()
       if recover_cursor_color == nil or type(recover_cursor_color) ~= 'table' then
         return
       end
 
       ---@diagnostic disable-next-line: param-type-mismatch
       vim.api.nvim_set_hl(0, 'Cursor', recover_cursor_color)
-    end
+    end,
   })
-
 
   -- Allow to paste from registers in fzf buffer in normal mode
   -- similar to ctrl-r in inser-mode
   vim.api.nvim_create_autocmd('FileType', {
     pattern = 'fzf',
     group = vim.api.nvim_create_augroup('PasteFzfBuffer', { clear = true }),
-    callback = function (opts)
+    callback = function(opts)
       vim.keymap.set('n', '<leader>"', function()
         vim.cmd('let @" = getreg(nr2char(getchar()))')
         return 'p'
@@ -87,7 +84,7 @@ local register = function()
   })
 
   vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function (opts)
+    callback = function(opts)
       local id = opts.data.client_id
       local client = vim.lsp.get_client_by_id(id)
 
