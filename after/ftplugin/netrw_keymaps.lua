c42ca1daf3422577fe796ac1be52328522a02479
daniel <daniel@mobile.com>
2025-11-20 09:25:02 -0500

    fix(netrw): prevent overriding existing files

--- after/ftplugin/netrw_keymaps.lua
+++ after/ftplugin/netrw_keymaps.lua
@@ -1,6 +1,10 @@
 local buf = vim.api.nvim_get_current_buf()
 
-vim.keymap.set('n', '%', function ()
+---Prompts and creates a file in netrw display root
+---@param opts { edit: boolean; }
+local function createFile(opts)
+  opts = opts or {}
+
   vim.ui.input({ prompt = 'Enter filename: ' }, function(input)
     local from_win = vim.g.custom_netrw_from_win
     if not input or not from_win or not vim.api.nvim_win_is_valid(from_win) then
@@ -13,7 +17,21 @@ vim.keymap.set('n', '%', function ()
       return
     end
 
-    vim.cmd.Lex({ bang = true })
-    vim.cmd.edit(vim.fs.joinpath(dir, input))
+    local new_file = vim.fs.joinpath(dir, input)
+
+    if vim.uv.fs_stat(new_file) then
+      vim.notify('File already exists', vim.log.levels.ERROR)
+      return
+    end
+
+
+    if opts.edit then
+      vim.cmd.Lex({ bang = true })
+      vim.cmd.edit(new_file)
+    else
+      vim.fn.writefile({}, new_file)
+    end
   end)
-end, { buffer = buf, noremap = true, desc = '[netrw] create file' })
+end
+
+vim.keymap.set('n', '%', createFile, { buffer = buf, noremap = true, desc = '[netrw] create file' })
