3a3bb4d59b1deb59583185dd1fffdbca031ccda3
daniel <eduardo.daniel.s.m@gmail.com>
2026-02-21 16:22:33 -0500

    fix(cwd): Set correct cwd when vscode launches

--- lua/vsc/autocmd.lua
+++ lua/vsc/autocmd.lua
@@ -1,5 +1,7 @@
 
 -- FIX: Treesitter failing to detect parser for textobjects
+-- If the issue somehow still happens, try using `]m` or `[m` to force
+-- loading the treesitter parser
 vim.api.nvim_create_autocmd('BufEnter', {
   group = vim.api.nvim_create_augroup('Treesitter.Buf.Enter', { clear = true }),
   callback = vim.schedule_wrap(function()
@@ -7,3 +9,22 @@ vim.api.nvim_create_autocmd('BufEnter', {
   end)
 })
 
+
+-- FIX: Initial path setup when starting in WSL
+-- with either windows nvim or linux nvim
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function (args)
+    coroutine.resume(coroutine.create(vim.schedule_wrap(function ()
+      local vscode = require('vscode')
+      local workspace = vscode.eval('return  vscode.workspace.workspaceFolder || vscode.workspace.workspaceFolders[0]')
+      -- Both WSL nvim and windows nvim use same scheme
+      local is_remote = workspace and workspace.uri and workspace.uri.scheme == 'vscode-remote'
+      if not is_remote then
+        return
+      end
+
+      local expanded = require('lib.fs').expand_path(workspace.uri.path)
+      pcall(vim.cmd.cd, expanded)
+    end)))
+  end,
+})
