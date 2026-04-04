959d39c371f86dd61162576abe0148cd2033fc23
daniel <eduardo.daniel.s.m@gmail.com>
2026-04-04 02:16:20 -0400

    style: formatting to file

--- lua/shared/autocmd.lua
+++ lua/shared/autocmd.lua
@@ -1,7 +1,7 @@
 -- Show yanked text
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('TextYankedGroup', { clear = true }),
-  callback = function ()
+  callback = function()
     vim.hl.on_yank({ higroup = 'HighlightYankedText' })
   end,
   desc = 'Highlight yanked text',
