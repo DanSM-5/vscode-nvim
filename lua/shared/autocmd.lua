e8209f1bf63390498d3a7a3f169c4447211d5486
daniel <eduardo.daniel.s.m@gmail.com>
2026-04-04 02:14:14 -0400

    fix(deprecate): change to vim.hl instead of vim.highlight

--- lua/shared/autocmd.lua
+++ lua/shared/autocmd.lua
@@ -2,7 +2,7 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('TextYankedGroup', { clear = true }),
   callback = function ()
-    vim.highlight.on_yank({ higroup = 'HighlightYankedText' })
+    vim.hl.on_yank({ higroup = 'HighlightYankedText' })
   end,
   desc = 'Highlight yanked text',
 })
