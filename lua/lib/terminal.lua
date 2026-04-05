b76527863c05cf039a29bed7b3c0228053102b97
daniel <eduardo.daniel.s.m@gmail.com>
2026-04-04 21:26:19 -0400

    feat(terminal): add float terminal function

--- lua/lib/terminal.lua
+++ lua/lib/terminal.lua
@@ -16,11 +16,26 @@
 ---@field stdin? 'pipe'|'null'
 ---@field clear_env? boolean
 
-local def_int_buf_name = 'interactive_terminal'
+local terminal_type = 'interactive_terminal'
+local terminal_const = {
+  interactive = 'interactive_term',
+  static = 'static_term',
+  filetype = 'lib_term',
+  name = 'lib_term',
+  term_win = 'term_win',
+  term_buf = 'term_buf',
+}
 
----Use lf in neovim to select and open files for editing
+local function safe_set_win_option(win, name, value)
+  pcall(vim.api.nvim_set_option_value, name, value, { win = win })
+end
+
+local function safe_set_buf_option(buf, name, value)
+  pcall(vim.api.nvim_set_option_value, name, value, { buf = buf })
+end
+
 ---@param opts? terminal.open_terminal
-local function open_terminal_interactive(opts)
+local function interactive_term(opts)
   opts = opts or {}
 
   if (not opts.cmd) or #opts.cmd == 0 then
@@ -58,8 +73,8 @@ local function open_terminal_interactive(opts)
       cwd = vim.fn.expand('~')
     end
   end
-  pcall(vim.api.nvim_set_option_value, 'filetype', opts.ft or def_int_buf_name, { buf = buf })
-  pcall(vim.api.nvim_set_option_value, 'buftype', opts.bt or def_int_buf_name, { buf = buf })
+  safe_set_buf_option(buf, 'filetype', opts.ft or terminal_type)
+  safe_set_buf_option(buf, 'buf', opts.bt or terminal_type)
 
   -- Enable insert mode on open
   vim.api.nvim_create_autocmd('BufEnter', {
@@ -151,6 +166,168 @@ local function open_terminal_interactive(opts)
   end)
 end
 
+---@class terminal.jobstart.opts
+---@field cwd? string Working directory to start the terminal
+---@field env? table<string, string> Pass to add variables to environment
+---@field clear_env? boolean Use clear environment instead of merging
+---@field on_exit? fun(jobId: integer, code: integer, event: 'exit')
+---@field on_stdout? fun(channelId: integer, data: string[], name: 'stdout')
+---@field on_stderr? fun(channelId: integer, data: string[], name: 'stderr')
+---@field stdin? 'pipe'|'null' string value 'pipe' to connect the job's stdin to a channel or 'null' to disconnect stdin
+---@field stderr_buffered? boolean collect data until EOF (stream close)
+---@field stdout_buffered? boolean collect data until EOF (stream close)
+
+---@class terminal.jobstart.int_opts: terminal.jobstart.opts
+---@field pty? boolean if this is a pty terminal
+---@field term? boolean spawns in pseudo terminal session. Implies 'pty'
+
+
+-- -@field on_stdin? fun(channelId: integer, data: string[], name: 'stdin')
+
+-- TODO: should on_end include std error?
+
+---@class terminal.opts
+---@field cmd string|string[] command to execute
+---@field term? terminal.jobstart.opts
+---@field float? vim.api.keyset.win_config
+---@field on_end? fun(lines: string[]) callback to be called at the end with full output
+---@field ft? string custom filetype to use
+---@field bt? string custom buftype to use
+
+
+---@class terminal.output.window
+---@field win integer winrn of floating window
+---@field buf integer bufnr of floating window
+
+---@class terminal.output
+---@field jobid integer jobId of the process
+---@field win integer winrn of floating window
+---@field buf integer bufnr of floating window
+---@field close fun() function to close the window
+
+---Get floating term config
+---@param config vim.api.keyset.win_config
+---@return vim.api.keyset.win_config
+local get_float_config = function(config)
+  local new_config = vim.tbl_deep_extend('force', {
+    relative = 'editor',
+    width = math.floor(vim.o.columns * 0.8),
+    height = math.floor(vim.o.lines * 0.8),
+    row = math.floor((vim.o.lines * 0.2) / 2),
+    col = math.floor(vim.o.columns * 0.1),
+    style = 'minimal',
+    border = 'single',
+  }, config)
+
+  return new_config
+end
+
+---Options for creating terminal buffer
+---@param opts terminal.opts
+---@return integer winrn
+---@return integer bufnr
+local function create_win_buf(opts)
+  local config = get_float_config(opts.float)
+  local buf = vim.api.nvim_create_buf(false, true)
+  local win = vim.api.nvim_open_win(buf, true, config)
+  return buf, win
+end
+
+---Set options on terminal
+---@param opts terminal.opts
+---@param out terminal.output.window
+local function set_options(opts, out)
+  local buf, win = out.buf, out.win
+
+  -- Set buffer and window variables
+  vim.api.nvim_buf_set_var(buf, terminal_const.term_buf, buf)
+  vim.api.nvim_win_set_var(win, terminal_const.term_win, win)
+  vim.api.nvim_win_set_var(win, 'breadcrumbs_ignore', 1)
+
+  -- buffer options
+  safe_set_buf_option(buf, 'buftype', 'nofile')
+  safe_set_buf_option(buf, 'bufhidden', 'hide')
+  safe_set_buf_option(buf, 'swapfile', false)
+  safe_set_buf_option(buf, 'modifiable', false)
+  safe_set_buf_option(buf, 'filetype', opts.ft or terminal_const.filetype)
+
+  safe_set_win_option(win, 'wrap', false)
+  safe_set_win_option(win, 'number', false)
+  safe_set_win_option(win, 'relativenumber', false)
+  safe_set_win_option(win, 'signcolumn', 'no')
+end
+
+---Validate options
+---@param opts terminal.opts
+local function validate_opts(opts)
+  opts = opts or {}
+  opts.float = opts.float or {}
+  opts.term = opts.term or {}
+end
+
+---@param opts terminal.opts options for the terminal
+---@return terminal.output output values to control float terminal
+local function float_term(opts)
+  validate_opts(opts)
+
+  local buf, win = create_win_buf(opts)
+  local term_opts = vim.tbl_deep_extend('force', opts.term or {}, { pty = true, term = true })
+  ---@cast term_opts terminal.jobstart.int_opts
+
+  set_options(opts, { win = win, buf = buf })
+
+  local term_autocmd_group = vim.api.nvim_create_augroup(('float_term_%d_%d'):format(win, buf), { clear = true })
+
+  vim.api.nvim_create_autocmd('TermOpen', {
+    once = true,
+    buffer = buf,
+    group = term_autocmd_group,
+    callback = function ()
+      vim.api.nvim_buf_call(buf, function ()
+        vim.cmd.startinsert()
+      end)
+    end
+  })
+
+  local id = vim.fn.jobstart(opts.cmd, {
+    cwd = term_opts.cwd,
+    env = term_opts.env,
+    clear_env = term_opts.clear_env,
+    pty = term_opts.pty,
+    term = term_opts.term,
+    on_stderr = term_opts.on_stderr,
+    on_stdout = term_opts.on_stdout,
+    stdin = term_opts.stdin,
+    stdout_buffered = term_opts.stdout_buffered,
+    stderr_buffered = term_opts.stderr_buffered,
+    on_exit = term_opts.on_exit,
+  })
+
+  local function close()
+    pcall(vim.fn.jobstop, id)
+    pcall(vim.api.nvim_win_close, win, true)
+    pcall(vim.api.nvim_clear_autocmds, { group = term_autocmd_group })
+    vim.cmd.checktime()
+  end
+
+  vim.api.nvim_create_autocmd('TermClose', {
+    once = true,
+    buffer = buf,
+    callback = function()
+      close()
+    end,
+  })
+
+  return {
+    close = close,
+    buf = buf,
+    win = win,
+    jobId = id,
+  }
+end
+
 return {
-  open_terminal_interactive = open_terminal_interactive,
+  interactive_term = interactive_term,
+  get_float_config = get_float_config,
+  float_term = float_term,
 }
