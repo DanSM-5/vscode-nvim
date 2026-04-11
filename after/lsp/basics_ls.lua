93cd8b4a5d489f723c1f56beefaba26080f443d9
daniel <eduardo.daniel.s.m@gmail.com>
2026-04-11 17:02:02 -0400

    refactor(basics_ls): load friendly snippets from pack

--- after/lsp/basics_ls.lua
+++ after/lsp/basics_ls.lua
@@ -8,6 +8,9 @@
 --- npm install -g basics-language-server
 --- ```
 
+local data = vim.fn.stdpath('data')
+local friendly_snippets = vim.fs.normalize(('%s/site/pack/core/opt/friendly-snippets/package.json'):format(data))
+
 ---@type vim.lsp.Config
 return {
   cmd = { 'basics-language-server' },
@@ -23,15 +26,16 @@ return {
     snippet = {
       enable = true,
       sources = {
-        vim.fn.substitute(vim.fn.stdpath('data'), '\\', '/', 'g') .. '/lazy/friendly-snippets/package.json'
+        -- vim.fn.substitute(vim.fn.stdpath('data'), '\\', '/', 'g') .. '/lazy/friendly-snippets/package.json'
+        friendly_snippets,
       },
       matchStrategy = 'fuzzy',
     },
   },
-  on_attach = function (client, bufnr)
+  on_attach = function(client, bufnr)
     require('utils.lsp_maps').set_lsp_keys(client, bufnr)
     require('utils.complete').configure(client, bufnr, {
-      triggerCharacters = vim.split('abcdefghijklmnopqrstuvwxyz./', '')
+      triggerCharacters = vim.split('abcdefghijklmnopqrstuvwxyz./', ''),
     })
   end,
 }
