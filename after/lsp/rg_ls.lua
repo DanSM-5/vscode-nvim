bc5e93c2f99ffcf1cfca80405e2eb27557236372
daniel <eduardo.daniel.s.m@gmail.com>
2026-04-18 14:00:08 -0400

    feat(rg_ls): add config to start rg_ls

---
+++ after/lsp/rg_ls.lua
@@ -0,0 +1,18 @@
+local name = 'rg_ls'
+
+return {
+  name = name,
+  on_attach = function(client, bufnr)
+    local triggerCharacters = vim.split('abcdefghijklmnopqrstuvwxyz', '')
+    require('utils.lsp_maps').set_lsp_keys(client, bufnr)
+    require('utils.complete').configure(client, bufnr, { triggerCharacters = triggerCharacters })
+  end,
+  cmd = function(...)
+    return require('lib.lsp.rg_ls').register(...)
+  end,
+  root_markers = { '.git' },
+  workspace_required = false,
+  ---@type rg.settings.user
+  settings = {
+  },
+}
